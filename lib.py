a574393fda309d2fee4c59e3535ad08671bc4c0f
Peter <peter@Peterde-MacBook-Pro.local>
2017-02-06 22:16:26 +0800

    libarary for decision plot and gen data

--- lib.py
+++ lib.py
@@ -1,8 +1,14 @@
 from matplotlib.colors import ListedColormap
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
 
-def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
+'''
+Parameters
+    ------------
+    soft : bool, optional
+        Whether to plot soft regions. Default is False. Only applicable to binary classifiers.
+'''
+def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02, soft=False):
 
     # setup marker generator and color map
     markers = ('s', 'x', 'o', '^', 'v')
@@ -14,13 +20,18 @@ def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
     x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
     xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
                            np.arange(x2_min, x2_max, resolution))
-    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
-    Z = Z.reshape(xx1.shape)
-    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
+    if soft:
+        Z = classifier.predict_proba(np.array([xx1.ravel(), xx2.ravel()]).T)[:, 0]
+        Z = Z.reshape(xx1.shape)
+        contour = plt.contourf(xx1, xx2, Z, alpha=0.4, camp=colors[0])
+        plt.colorbar(contour)
+    else:
+        Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+        Z = Z.reshape(xx1.shape)
+        plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
     plt.xlim(xx1.min(), xx1.max())
     plt.ylim(xx2.min(), xx2.max())
 
-    # plot class samples
     for idx, cl in enumerate(np.unique(y)):
         plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1],
                     alpha=0.8, c=cmap(idx),
@@ -28,7 +39,6 @@ def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
 
     # highlight test samples
     if test_idx:
-        # plot all samples
         X_test, y_test = X[test_idx, :], y[test_idx]
 
         plt.scatter(X_test[:, 0],
@@ -38,3 +48,8 @@ def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
                     linewidths=1,
                     marker='o',
                     s=55, label='test set')
+
+def gen_data(num_data, sigma):
+    x = 2 * np.pi * (np.random.rand(num_data) - 0.5)
+    y = np.sin(x) + np.random.normal(0, sigma, num_data)
+    return (x, y)
